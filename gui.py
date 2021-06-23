7ec29df8a98100eca7103600165efd8b6c6736c5
巫淨翡翠 <36731917+FujouHisui@users.noreply.github.com>
2021-06-23 17:12:04 +0800

    gui of confirm and student pages

--- gui.py
+++ gui.py
@@ -4,6 +4,8 @@ from PyQt5.QtWidgets import QWidget, QApplication
 
 from password import Ui_Password
 from welcome import Ui_Welcome
+from student import Ui_StuInfo
+from confirm import Ui_Confirm
 
 
 class WelcomeForm(QWidget, Ui_Welcome):
@@ -26,6 +28,32 @@ class PasswordForm(QWidget, Ui_Password):
 
     def show_pf(self):
         self.show()
+
+    def yesButton_Click(self):
+        self.sf = StudentForm()
+        self.sf.show()
+        self.close()
+
+    def noButton_Click(self):
+        self.close()
+
+
+class StudentForm(QWidget, Ui_StuInfo):
+    def __init__(self):
+        super(StudentForm, self).__init__()
+        self.setupUi(self)
+
+    def show_sf(self):
+        self.show()
+
+    def backButton_Click(self):
+        self.close()
+
+
+class ConfirmForm(QWidget, Ui_Confirm):
+    def __init__(self):
+        super(ConfirmForm, self).__init__()
+        self.setupUi(self)
 
     def yesButton_Click(self):
         self.close()
