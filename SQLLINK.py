b62522dae1fdc96acc47cb8c1486afca4287e350
VoidFox-Mask <1317855214@qq.com>
2021-06-25 15:39:46 +0800

    SQL, legit_data

--- SQLLINK.py
+++ SQLLINK.py
@@ -1,31 +1,15 @@
-import re
-
 import pymysql
 
-'''
-#列表
-# 创建链接对象
-conn = pymysql.connect(host='la.hisui.tech', port=3306, user='root', passwd='Yz1UBRM3>qx2_Q+7j#zZ', db='library')
-# 创建游标
-cursor = conn.cursor()
-# 执行sql，更新单条数据，并返回受影响行数
-cursor.execute("SELECT * from user_table")
-result = cursor.fetchall()
-print(result)
-# 关闭游标
-cursor.close()
-# 关闭连接
-conn.close()
-'''
 
 
-# 增
+# 增,添加借书记录
 def log_add(stu_id, operation, book_id):
     conn = pymysql.connect(host='la.hisui.tech', port=3306, user='root', passwd='Yz1UBRM3>qx2_Q+7j#zZ', db='library')
     cursor = conn.cursor()
     try:
         cursor.execute('''INSERT INTO library.borrow_log (stu_id, book_id, operation, timestamp)
 VALUES (''' + stu_id + ''', ''' + operation + ''', ''' + book_id + ''', DEFAULT)''')
+        conn.commit()
     except pymysql.err.OperationalError:
         print("输入数据有误")
         # 关闭游标
@@ -85,6 +69,25 @@ def book_status_change():
     cursor.close()
     # 关闭连接
     conn.close()
+
+
+
+def search_stu_date(stu_id):  # 输入学号查用户详情，返回用户名和学号
+    sql_select("uname,passwd,stu_id", "user_table", "stu_id = " + stu_id)
+
+
+def search_borrow_log(stu_id):  # 查询借书记录，输入学生学号,返回书的id和书的名字
+    sql_select("borrow_log.book_id,book_table.book_name", "borrow_log inner join book_table",
+               "borrow_log.stu_id = " + stu_id)
+
+
+def search_needretrun(stu_id):  # 输入学号查询代还书籍
+    sql_select("borrow_log.book_id,book_table.book_name", "borrow_log inner join book_table",
+               "book.table.is_borrowed = 1 and borrow_log.stu_id = " + stu_id)
+
+
+def search_borrow_state(book_id):  # 查询书的状态，输入书的ID，返回1为被借出，返回0为未被借出
+    sql_select("is_borrowed", "book_table", "book = " + book_id)
 
 
 def search_passwd(identity, user):
